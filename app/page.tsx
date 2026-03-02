8529653e1e331f040c8107917b907a8ca0d73b41
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2026-03-02 16:44:54 +0100

    feat: add github workflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import { Faq } from "@/components/landing/faq";
 import { Hero } from "@/components/landing/hero";
+import { Footer } from "@/components/landing/footer";
 import { StepsAway } from "@/components/landing/steps-away";
 import { DownloadOurApp } from "@/components/landing/download-our-app";
 import { EarnOnYourTerms } from "@/components/landing/earn-on-your-terms";
 import { QualityYouCanTrust } from "@/components/landing/quality-you-can-trust";
-import { Footer } from "@/components/landing/footer";
 
 export default function HomePage() {
     return (
