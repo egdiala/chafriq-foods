70fb58318fd452ab282f8d89be27bcbc4298714a
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2026-03-09 00:40:17 +0100

    feat: header dropdown

--- components/vendor/header.tsx
+++ components/vendor/header.tsx
@@ -5,9 +5,9 @@ import { Content } from "../content";
 import { NavItem } from "./nav-item";
 import { Button } from "../ui/button";
 import { type RouteType } from "next/dist/lib/load-custom-routes";
-import { IconArrowDown, IconBell, IconBowlSteam, IconChartBar, IconChartLine, IconHamMenu, IconStorefront, LogoWithText } from "../icons";
-import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { IconArrowDown, IconBell, IconBowlSteam, IconChartBar, IconChartLine, IconChefHat, IconCurrencyDollar, IconHamMenu, IconHeadset, IconSignOut, IconStorefront, IconUser, LogoWithText } from "../icons";
 
 export const VendorHeader = () => {
     const headerLinks = [
@@ -49,6 +49,39 @@ export const VendorHeader = () => {
                                 <span className="font-medium text-sm text-grey-dark-2">Diala</span>
                                 <IconArrowDown className="transition-transform duration-200 ease-linear" />
                             </DropdownMenuTrigger>
+                            <DropdownMenuContent className="p-2.5! w-50 origin-top-right space-y-2" align="end" sideOffset={8}>
+                                <div className="flex items-center gap-2">
+                                    <Avatar size="lg">
+                                        <AvatarImage src="https://github.com/shadcn.png" className="rounded-xl" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <div className="grid gap-px">
+                                        <span className="font-medium text-sm text-grey-dark-1">Stephen Diala</span>
+                                        <div className="flex items-center px-1 gap-1 bg-orange-5 rounded-xl h-4.5 w-fit uppercase text-orange-2 text-[0.625rem] [&>svg]:size-3">
+                                            <IconChefHat /> cook
+                                        </div>
+                                    </div>
+                                </div>
+                                <DropdownMenuSeparator className="mx-px" />
+                                <DropdownMenuGroup className="mt-2">
+                                    <DropdownMenuItem className="[&>svg]:text-orange-2 not-data-[variant=destructive]:focus:**:text-orange-2!">
+                                        <IconUser />
+                                        View Profile
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem className="[&>svg]:text-orange-2 not-data-[variant=destructive]:focus:**:text-orange-2!">
+                                        <IconCurrencyDollar />
+                                        Payout
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem className="[&>svg]:text-orange-2 not-data-[variant=destructive]:focus:**:text-orange-2!">
+                                        <IconHeadset />
+                                        Support
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem className="[&>svg]:text-orange-2 not-data-[variant=destructive]:focus:**:text-orange-2!">
+                                        <IconSignOut />
+                                        Logout
+                                    </DropdownMenuItem>
+                                </DropdownMenuGroup>
+                            </DropdownMenuContent>
                         </DropdownMenu>
                     </div>
                 </div>
