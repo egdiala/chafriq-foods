2c911608528021f3970e1e3aaa127373b121c3e0
Egwuchukwu Stephen Diala <dialaegwuchukwu@gmail.com>
2026-03-26 22:42:19 +0100

    fix: build issues

--- trpc/helper.ts
+++ trpc/helper.ts
@@ -11,53 +11,53 @@ export const api = axios.create({
     },
 });
 
-const refreshToken = async (access_token: string, refresh_token: string) => {
-    const response = await api.post("auth/refresh", { refresh_token }, {
-        headers: {
-          "Authorization": `Bearer ${access_token}`
-        }
-      });
-    if (!response.data) {
-        throw new Error("FAILED_TO_REFRESH_TOKEN");
-    }
-    return response.data as RefreshTokenResponse;
-};
+// const refreshToken = async (access_token: string, refresh_token: string) => {
+//     const response = await api.post("auth/refresh", { refresh_token }, {
+//         headers: {
+//           "Authorization": `Bearer ${access_token}`
+//         }
+//       });
+//     if (!response.data) {
+//         throw new Error("FAILED_TO_REFRESH_TOKEN");
+//     }
+//     return response.data as RefreshTokenResponse;
+// };
 
-api.interceptors.response.use((response) => response, async (error) => {
-    const config = error.config;
+// api.interceptors.response.use((response) => response, async (error) => {
+//     const config = error.config;
 
-    // Check that the error is an unauthorized, the request has not already been retried, and we have a refreshToken
-    if (error?.response?.status === 401) {
-        config._retry = true; // mark that we have retried this request
-        const cookieStore = await cookies();
-        const accessTokenValue = cookieStore.get("access_token")?.value;
-        const refreshTokenValue = cookieStore.get("refresh_token")?.value;
+//     // Check that the error is an unauthorized, the request has not already been retried, and we have a refreshToken
+//     if (error?.response?.status === 401) {
+//         config._retry = true; // mark that we have retried this request
+//         const cookieStore = await cookies();
+//         const accessTokenValue = cookieStore.get("access_token")?.value;
+//         const refreshTokenValue = cookieStore.get("refresh_token")?.value;
 
-        if (refreshTokenValue && accessTokenValue) {
-            try {
-                const response = await refreshToken(accessTokenValue, refreshTokenValue);
+//         if (refreshTokenValue && accessTokenValue) {
+//             try {
+//                 const response = await refreshToken(accessTokenValue, refreshTokenValue);
 
-                if (response.access_token && response.refresh_token) {
-                    setCredentials({
-                        access_token: response.access_token,
-                        refresh_token: response.refresh_token,
-                    });
-                    // Retry original request with new JWT
-                    config.headers = {
-                        ...config.headers,
-                        Authorization: `Bearer ${response.access_token}`,
-                    };
-                    return api.request(config);
-                }
-            } catch (refreshErr) {
-                await clearCredentials();
-                return Promise.reject(refreshErr);
-            }
-        }
-    }
+//                 if (response.access_token && response.refresh_token) {
+//                     setCredentials({
+//                         access_token: response.access_token,
+//                         user_type: response.refresh_token,
+//                     });
+//                     // Retry original request with new JWT
+//                     config.headers = {
+//                         ...config.headers,
+//                         Authorization: `Bearer ${response.access_token}`,
+//                     };
+//                     return api.request(config);
+//                 }
+//             } catch (refreshErr) {
+//                 await clearCredentials();
+//                 return Promise.reject(refreshErr);
+//             }
+//         }
+//     }
 
-    return Promise.reject(error);
-});
+//     return Promise.reject(error);
+// });
 
 export const handleErrorMessage = (error: any) => {
     return (error?.response?.data?.error || error?.response?.data?.message || error?.message || "An unknown error occurred");
